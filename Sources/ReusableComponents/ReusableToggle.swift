ef53b1bf5a7a6f4c142a705caee945cf89b6dd2b
Venkatesh Polepalli <Venkatesh.polepalli@innovasolutions.com>
2026-03-11 15:11:22 +0530

    feat(toggle): toggle updates

--- Sources/ReusableComponents/ReusableToggle.swift
+++ Sources/ReusableComponents/ReusableToggle.swift
@@ -7,33 +7,32 @@
 
 import SwiftUI
 
-public struct ReusableToggle<Label: View>: View {
+public struct ReusableToggle: View {
+    private let title: String
     private let isOn: Binding<Bool>
     private let accessibilityId: String?
-    private let label: Label
 
     public init(
         title: String,
         isOn: Binding<Bool>,
         accessibilityId: String? = nil
-    ) where Label == Text {
+    ) {
+        self.title = title
         self.isOn = isOn
         self.accessibilityId = accessibilityId
-        self.label = Text(title)
     }
 
     public init(
         isOn: Binding<Bool>,
-        accessibilityId: String? = nil,
-        @ViewBuilder label: () -> Label
+        accessibilityId: String? = nil
     ) {
+        self.title = ""
         self.isOn = isOn
         self.accessibilityId = accessibilityId
-        self.label = label()
     }
 
     public var body: some View {
-        Toggle(isOn: isOn) { label }
+        Toggle(title, isOn: isOn)
             .reusableAccessibilityIdentifier(accessibilityId)
     }
 }
