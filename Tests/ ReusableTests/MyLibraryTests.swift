c2775a8c935057fa0f84e10724fd71952e3ed643
Venkatesh Polepalli <Venkatesh.polepalli@innovasolutions.com>
2026-03-17 14:41:23 +0530

    feat(tests): minor updates

--- Tests/ ReusableTests/MyLibraryTests.swift
+++ Tests/ ReusableTests/MyLibraryTests.swift
@@ -18,7 +18,7 @@ final class SwiftElementsTests: XCTestCase {
     @MainActor
     func testReusableTextField_buildsBody() {
         let text = Binding<String>(get: { "" }, set: { _ in })
-        let sut = ReusableTextField(text: text, style: .roundedBorder, prompt: Text("Enter name"), accessibilityId: "name_field")
+        let sut = ReusableTextField(text: text, style: .roundedBorder, prompt: "Enter name", accessibilityId: "name_field")
         _ = sut.body
     }
 
